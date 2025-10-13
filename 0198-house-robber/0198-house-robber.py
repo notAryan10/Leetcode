be64ab283db5ccce254f2a73c39fbd478ed31ee5
Aryan Verma <aryanverma1857@gmail.com>
2025-10-13 22:22:27 +0530

    Time: 0 ms (100%), Space: 18.1 MB (13.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def maxSum(n,nums):
-            if n == 0:
+        n = len(nums)
+        memo = {}
+        def func(i):
+            if i >= n:
                 return 0
-            if n == 1:
-                return nums[0]
-            
-            dp2 = nums[0]
-            dp1 = max(nums[0], nums[1])
-
-            for i in range(2, n):
-                curr = max(dp1, nums[i] + dp2)
-                dp2, dp1 = dp1, curr
-            return dp1
-        return maxSum(len(nums), nums)
+            if i in memo:
+                return memo[i]
+            take = nums[i] + func(i + 2)
+            skip = func(i + 1)
+            memo[i] = max(take, skip)
+            return memo[i]
+        return func(0) 
